4575129684babb8afc84f42fde649ebf749521c6
Wael Elkadim <wael.elkadim@microsoft.com>
2024-04-17 11:49:03 +0200

    Add project files.

---
+++ ApplicationSample.Web/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿namespace ApplicationSample.Web.Models
+{
+    public class Customer
+    {
+       public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public bool IsActive { get; set; }
+        
+        public byte[] IdPhoto { get; set; }
+
+        public string IdPhotoContentType { get; set; }
+    }
+}
