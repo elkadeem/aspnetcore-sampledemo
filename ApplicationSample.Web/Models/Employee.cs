fa0485edcffe65e3d79b21555897ae220fa21ae7
Wael Elkadim <wael.elkadim@microsoft.com>
2024-04-22 10:56:19 +0200

    Model employee and department entities

---
+++ ApplicationSample.Web/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationSample.Web.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public Name Name { get; set; }
+
+        [MaxLength(50)]
+        public string EmployeeNumber { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [MaxLength(20)]
+        public string NationalId { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        public byte[]? IdPhoto { get; set; }
+
+        [MaxLength(50)]
+        public string? IdPhotoContentType { get; set; }
+
+        [MaxLength(100)] 
+        public string Email { get; set; }
+
+        [MaxLength(20)] 
+        public string Mobile { get; set; }
+
+        public Deparment Deparment { get; set; }
+
+    }
+}
---
+++ ApplicationSample.Web/Models/Name.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationSample.Web.Models
+{
+    public class Name
+    {
+        public Name(string firstName, string fatherName, string middleName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentException($"'{nameof(firstName)}' cannot be null or whitespace.", nameof(firstName));
+            }
+
+            if (string.IsNullOrWhiteSpace(fatherName))
+            {
+                throw new ArgumentException($"'{nameof(fatherName)}' cannot be null or whitespace.", nameof(fatherName));
+            }
+
+            if (string.IsNullOrWhiteSpace(middleName))
+            {
+                throw new ArgumentException($"'{nameof(middleName)}' cannot be null or whitespace.", nameof(middleName));
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentException($"'{nameof(lastName)}' cannot be null or whitespace.", nameof(lastName));
+            }
+
+            FirstName = firstName;
+            LastName = lastName;
+            FatherName = fatherName;
+            MiddleName = middleName;
+        }
+
+        [MaxLength(50)]
+        public string FirstName { get; private set; }
+
+        [MaxLength(50)]
+        public string LastName { get; private set; }
+
+        [MaxLength(50)]
+        public string FatherName { get; private set; }
+
+        [MaxLength(50)]
+        public string MiddleName { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {FatherName} {MiddleName} {LastName}";
+        }
+    }
+}
