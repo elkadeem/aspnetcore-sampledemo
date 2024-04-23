2bce58d650da66b6ff1e437e6456b60ea92a91df
Wael Elkadim <wael.elkadim@microsoft.com>
2024-04-23 13:52:29 +0200

    Add manual pager without handling any improvments

--- ApplicationSample.Web/Pages/Customers/Index.cshtml.cs
+++ ApplicationSample.Web/Pages/Customers/Index.cshtml.cs
@@ -1,4 +1,6 @@
 ﻿using ApplicationSample.Web.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,19 +16,32 @@ namespace ApplicationSample.Web.Pages.Customers
         }
 
         public IList<CustomerDto> Customers { get;set; } = default!;
+        public int PageSize { get; set; } = 3;
+        [BindProperty(SupportsGet = true)]
+        public int PageNumber { get; set; } = 1;
 
+        public int TotalItemsCount { get; set; }
         public async Task OnGetAsync()
         {
-            Customers = await _context.Customers
-                .Select(c => new CustomerDto {
-                  Address = c.Address,
-                  BirthDay = c.BirthDay,
-                  Email = c.Email,
-                  Id = c.Id,
-                  IsActive = c.IsActive,
-                  Name = c.Name,
-                  Phone = c.Phone                  
-                 }).ToListAsync();
+            PageNumber = Math.Max(1, PageNumber);
+            var query = _context.Customers
+                .Select(c => new CustomerDto
+                {
+                    Address = c.Address,
+                    BirthDay = c.BirthDay,
+                    Email = c.Email,
+                    Id = c.Id,
+                    IsActive = c.IsActive,
+                    Name = c.Name,
+                    Phone = c.Phone
+                });
+
+            TotalItemsCount = await query.CountAsync();
+            Customers = await query
+                .OrderBy(c => c.Name)
+                .Skip((PageNumber -1) * PageSize)
+                .Take(PageSize)
+                .ToListAsync();
         }
     }
 }
