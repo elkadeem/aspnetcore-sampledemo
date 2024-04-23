90ea2b4aa9cd977b2c55a2a8ddc734ec60410ea2
Wael Elkadim <wael.elkadim@microsoft.com>
2024-04-23 13:27:59 +0200

    Add API Sample

--- ApplicationSample.Web/Program.cs
+++ ApplicationSample.Web/Program.cs
@@ -16,6 +16,9 @@ namespace ApplicationSample.Web
             builder.Services               
                 .AddRazorPages();
 
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -24,6 +27,11 @@ namespace ApplicationSample.Web
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+            else
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
